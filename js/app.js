9e4a725caade1881580a2e0b20613d0af8e68bf3
Tim Yates <tim@gradle.com>
2020-04-14 21:41:33 +0100

    calm down colours now the kids are away

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 const container = document.getElementById('container');
 const logo = document.getElementById('logo');
-const dw = document.documentElement.clientWidth / 2.0;
+const dw = document.body.clientWidth / 2.0;
 var counter = 0;
 const updateRate = 10;
 const isTimeToUpdate = () => counter++ % updateRate === 0;
 const update = e => {
     const x = (2.0 * (e.x - dw)) / dw;
-    const style = "rotateX(0deg) rotateY(" + x.toFixed(2) + "deg)";
+    const style = "rotateX(0deg) rotateY(" + (x * 30).toFixed(2) + "deg)";
     logo.style.transform = style;
 };
 const onMouseMoveHandler = event => { if (isTimeToUpdate()) { update(event); } };
