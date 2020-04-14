d8db2e9ecf1b433a30b9636e05fb7c990c6f8038
Tim Yates <tim@gradle.com>
2020-04-14 21:50:23 +0100

    rotate two ways

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 const container = document.getElementById('container');
 const logo = document.getElementById('logo');
 const dw = document.body.clientWidth / 2.0;
+const dh = document.body.clientHeight / 2.0;
 var counter = 0;
 const updateRate = 10;
 const isTimeToUpdate = () => counter++ % updateRate === 0;
 const update = e => {
     const x = (2.0 * (e.x - dw)) / dw;
-    const style = "rotateX(0deg) rotateY(" + (x * 30).toFixed(2) + "deg)";
+    const y = -(2.0 * (e.y - dh)) / dh;
+    const style = "rotateX(" + (y * 30).toFixed(2) + "deg) rotateY(" + (x * 30).toFixed(2) + "deg)";
     logo.style.transform = style;
 };
 const onMouseMoveHandler = event => { if (isTimeToUpdate()) { update(event); } };
-container.onmousemove = onMouseMoveHandler
-container.ontouchmove = onMouseMoveHandler
-container.onmouseout = e => logo.style.transform = "rotateX(0deg) rotateY(0deg)";
+document.body.onmousemove = onMouseMoveHandler
+document.body.ontouchstart = onMouseMoveHandler
+document.body.ontouchmove = onMouseMoveHandler
+document.body.onmouseout = e => logo.style.transform = "rotateX(0deg) rotateY(0deg)";
+
