6b8ecdda22316d0a5b7887081ab779b179a4755e
Tim Yates <tim@gradle.com>
2020-04-14 22:03:09 +0100

    touches

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 const container = document.getElementById('container');
 const logo = document.getElementById('logo');
-const dw = document.body.clientWidth / 2.0;
-const dh = document.body.clientHeight / 2.0;
 var counter = 0;
 const updateRate = 10;
 const isTimeToUpdate = () => counter++ % updateRate === 0;
 const update = e => {
-    const x = (2.0 * (e.x - dw)) / dw;
-    const y = -(2.0 * (e.y - dh)) / dh;
+    const dw = document.body.clientWidth / 2.0;
+    const dh = document.body.clientHeight / 2.0;
+    const currentX = e.touches ?  e.touches[0].pageX : e.pageX;
+    const currentY = e.touches ?  e.touches[0].pageY : e.pageY;
+    const x = (2.0 * (currentX - dw)) / dw;
+    const y = -(2.0 * (currentY - dh)) / dh;
     const style = "rotateX(" + (y * 30).toFixed(2) + "deg) rotateY(" + (x * 30).toFixed(2) + "deg)";
     logo.style.transform = style;
 };
